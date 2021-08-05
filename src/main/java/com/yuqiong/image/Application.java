c828c55239a37387e78581ffdc657117573e9bcc
qinjiawei1 <qinjiawei@nbdeli.com>
2021-08-05 16:04:22 +0800

    补充说明

---
+++ src/main/java/com/yuqiong/image/Application.java
@@ -0,0 +1,11 @@
+package com.yuqiong.image;
+
+/**
+ * @author DWL
+ * @date 2021/8/5
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
+}
